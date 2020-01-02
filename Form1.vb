3fd7cb67530c508753ff27e13e226d5a1a626af6
opickjey <opickjey@gmail.com>
2020-01-02 10:23:00 +0800

    Ubah

---
+++ Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tb_pass.UseSystemPasswordChar = True
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If tb_usr.Text = "admin" And tb_pass.Text = "12345" Then
+            MsgBox("Selamat anda berhasil login")
+            transaksi.Show()
+            Me.Hide()
+        Else
+            MsgBox("Username atau Password yang anda masukan salah")
+
+        End If
+    End Sub
+
+    Private Sub cb_show_CheckedChanged(sender As Object, e As EventArgs) Handles cb_show.CheckedChanged
+        If cb_show.Checked = True Then
+
+            tb_pass.UseSystemPasswordChar = False
+
+
+
+        Else
+            cb_show.Checked = False
+            tb_pass.UseSystemPasswordChar = True
+
+
+        End If
+    End Sub
+End Class
