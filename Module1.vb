d14f5faf25d587efc4ea04f23d0bc4875875aa08
opickjey <opickjey@gmail.com>
2020-01-02 10:13:36 +0800

    initial commit

---
+++ Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Data.SqlClient
+
+Module Module1
+    Public Conn As SqlConnection
+    Public Da As SqlDataAdapter
+    Public Ds As DataSet
+    Public CMD As SqlCommand
+    Public Dr As SqlDataReader
+
+    Public Sub koneksi()
+        Conn = New SqlConnection("Data Source=DESKTOP-6U85BAF\SQLEXPRESS;Integrated Security=True")
+        Conn.Open()
+
+    End Sub
+End Module
