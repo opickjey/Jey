9ee708d7508e0956e8a9799ea40548a229223305
opickjey <opickjey@gmail.com>
2020-01-08 15:25:05 +0800

    Menambahkan Form login

---
+++ login_edit.vb
@@ -0,0 +1,15 @@
+﻿Public Class login_edit
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If id.Text = "login" And pass.Text = "12345" Then
+            MsgBox("Selamat")
+            Me.Hide()
+            Barang.Show()
+
+        Else
+            MsgBox("UserID atau Pass yang anda masukan salah")
+
+
+        End If
+    End Sub
+End Class
