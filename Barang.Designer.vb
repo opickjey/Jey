e762fefee25a0a8aa4f4f3fc27d0ef62dc1689f3
opickjey <opickjey@gmail.com>
2020-01-08 15:17:57 +0800

    Psuh

---
+++ Barang.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Barang
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Barang))
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.B_tutup = New System.Windows.Forms.Button()
+        Me.B_batal = New System.Windows.Forms.Button()
+        Me.Hapus = New System.Windows.Forms.Button()
+        Me.B_simpan = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cb_satuan = New System.Windows.Forms.ComboBox()
+        Me.Dgv = New System.Windows.Forms.DataGridView()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tb_harga = New System.Windows.Forms.TextBox()
+        Me.tnm_barang = New System.Windows.Forms.TextBox()
+        Me.Tkd_brng = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.Dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox3.Location = New System.Drawing.Point(3, 16)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(194, 81)
+        Me.TextBox3.TabIndex = 0
+        '
+        'B_tutup
+        '
+        Me.B_tutup.Location = New System.Drawing.Point(119, 34)
+        Me.B_tutup.Name = "B_tutup"
+        Me.B_tutup.Size = New System.Drawing.Size(75, 23)
+        Me.B_tutup.TabIndex = 0
+        Me.B_tutup.Text = "Tutup"
+        Me.B_tutup.UseVisualStyleBackColor = True
+        '
+        'B_batal
+        '
+        Me.B_batal.Location = New System.Drawing.Point(119, 63)
+        Me.B_batal.Name = "B_batal"
+        Me.B_batal.Size = New System.Drawing.Size(75, 23)
+        Me.B_batal.TabIndex = 0
+        Me.B_batal.Text = "Batal"
+        Me.B_batal.UseVisualStyleBackColor = True
+        '
+        'Hapus
+        '
+        Me.Hapus.Location = New System.Drawing.Point(27, 63)
+        Me.Hapus.Name = "Hapus"
+        Me.Hapus.Size = New System.Drawing.Size(75, 23)
+        Me.Hapus.TabIndex = 0
+        Me.Hapus.Text = "Hapus"
+        Me.Hapus.UseVisualStyleBackColor = True
+        '
+        'B_simpan
+        '
+        Me.B_simpan.Location = New System.Drawing.Point(27, 34)
+        Me.B_simpan.Name = "B_simpan"
+        Me.B_simpan.Size = New System.Drawing.Size(75, 23)
+        Me.B_simpan.TabIndex = 0
+        Me.B_simpan.Text = "Simpan"
+        Me.B_simpan.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Location = New System.Drawing.Point(510, 132)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox2.TabIndex = 17
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Cari Data"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.B_tutup)
+        Me.GroupBox1.Controls.Add(Me.B_batal)
+        Me.GroupBox1.Controls.Add(Me.Hapus)
+        Me.GroupBox1.Controls.Add(Me.B_simpan)
+        Me.GroupBox1.Location = New System.Drawing.Point(513, 235)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 139)
+        Me.GroupBox1.TabIndex = 16
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Tools"
+        '
+        'cb_satuan
+        '
+        Me.cb_satuan.FormattingEnabled = True
+        Me.cb_satuan.Location = New System.Drawing.Point(170, 102)
+        Me.cb_satuan.Name = "cb_satuan"
+        Me.cb_satuan.Size = New System.Drawing.Size(121, 21)
+        Me.cb_satuan.TabIndex = 15
+        '
+        'Dgv
+        '
+        Me.Dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Dgv.Location = New System.Drawing.Point(58, 201)
+        Me.Dgv.Name = "Dgv"
+        Me.Dgv.Size = New System.Drawing.Size(433, 156)
+        Me.Dgv.TabIndex = 14
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(64, 139)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(73, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Harga Barang"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(64, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Nama Barang"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(64, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Kode Barang"
+        '
+        'tb_harga
+        '
+        Me.tb_harga.Location = New System.Drawing.Point(170, 132)
+        Me.tb_harga.Name = "tb_harga"
+        Me.tb_harga.Size = New System.Drawing.Size(100, 20)
+        Me.tb_harga.TabIndex = 7
+        '
+        'tnm_barang
+        '
+        Me.tnm_barang.Location = New System.Drawing.Point(170, 73)
+        Me.tnm_barang.Name = "tnm_barang"
+        Me.tnm_barang.Size = New System.Drawing.Size(100, 20)
+        Me.tnm_barang.TabIndex = 8
+        '
+        'Tkd_brng
+        '
+        Me.Tkd_brng.Location = New System.Drawing.Point(170, 41)
+        Me.Tkd_brng.Name = "Tkd_brng"
+        Me.Tkd_brng.Size = New System.Drawing.Size(100, 20)
+        Me.Tkd_brng.TabIndex = 9
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.Location = New System.Drawing.Point(687, 380)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "Kembali"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(64, 108)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(41, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Satuan"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label6.Location = New System.Drawing.Point(668, 13)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(41, 15)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "Label6"
+        '
+        'Timer1
+        '
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label7.Location = New System.Drawing.Point(576, 13)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(41, 15)
+        Me.Label7.TabIndex = 20
+        Me.Label7.Text = "Label7"
+        '
+        'Barang
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.ClientSize = New System.Drawing.Size(774, 399)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.cb_satuan)
+        Me.Controls.Add(Me.Dgv)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tb_harga)
+        Me.Controls.Add(Me.tnm_barang)
+        Me.Controls.Add(Me.Tkd_brng)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Barang"
+        Me.Text = "Barang"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.Dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents B_tutup As System.Windows.Forms.Button
+    Friend WithEvents B_batal As System.Windows.Forms.Button
+    Friend WithEvents Hapus As System.Windows.Forms.Button
+    Friend WithEvents B_simpan As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents cb_satuan As System.Windows.Forms.ComboBox
+    Friend WithEvents Dgv As System.Windows.Forms.DataGridView
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents tb_harga As System.Windows.Forms.TextBox
+    Friend WithEvents tnm_barang As System.Windows.Forms.TextBox
+    Friend WithEvents Tkd_brng As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+End Class
